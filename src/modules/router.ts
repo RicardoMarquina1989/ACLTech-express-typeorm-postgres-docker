3d0023594003a52d79fc104315497413de203185
ricardomarquina1989 <ricardoandresmarquinamolina92@gmail.com>
2024-11-08 20:32:26 -0300

    integrate url router to appRouter

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { HealthController } from './health/health.controller';
 import { QueueRouter } from './queue/queue.controller';
 import { TechnologyController } from './technology/technology.controller';
+import { UrlController } from './url/url.controller';
 
 const router = Router();
 
 router.use('/health', HealthController.router);
 router.use('/technology', TechnologyController.router);
 router.use('/queue', QueueRouter.router);
+router.use('/url', UrlController.router);
 
 export const apiRouter = router;
